ef3d072e665168e5363aa957438e266f307ce099
KHH0618 <dlow123@naver.com>
2024-09-26 14:56:02 +0900

    $(powershell -Command Get-Date -Format 'yyyy-MM-dd')

--- AUTOgit.bat
+++ AUTOgit.bat
@@ -1,6 +1,6 @@
 git pull origin main
 git add .
-git commit -m "$(Get-Date -Format 'yyyy-MM-dd')"
+git commit -m "$(powershell -Command Get-Date -Format 'yyyy-MM-dd')"
 git push origin main
 
 pause
