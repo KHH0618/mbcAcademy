698eaca4a5c39f8ee2911f6762392bc9b9e09a79
KHH618 <dlow123@naver.com>
2025-02-25 16:42:08 +0900

    2025-02-25

--- source/14_django/myproject/book/views.py
+++ source/14_django/myproject/book/views.py
@@ -1,26 +1,36 @@
 from django.shortcuts import render, redirect
+
+from .forms import BookForm, BookModelForm
 from .models import Book
 from django.views.generic import ListView
 from django.views.generic import CreateView
 from django.views.generic import UpdateView
-from django.views.generic import DetailView
-
-# def book_list(request):
-#     return render(request,
-#                   'book/book_list.html',
-#                   {'book_list':Book.objects.all()})
+from django.views.generic import DeleteView
 
 book_list = ListView.as_view(model=Book)
+# def book_list(request):
+#   return render(request,
+#                 'book/book_list.html',
+#                 {'book_list':Book.objects.all()})
 
 def book_new(request):
-    if request.method == "POST":
-        # request.POST의 파라미터 값을 book으로 save()
-        return redirect('book:list')
-    else :
-        return render(request, 'book/book_form.html')
+  if request.method == 'POST':
+    # request.POST의 파라미터 값을 book으로 save()
+    form = BookModelForm(request.POST, request.FILES)
+    if form.is_valid():
+      book = Book(**form.cleaned_data)
+      book.ip = request.META['REMOTE_ADDR'] # 요청한 client의 ip
+      book.save()
+      return redirect('book:list')
+  else:
+    #form = BookForm()
+    form = BookModelForm()
+  return render(request,
+                  'book/book_form.html',
+                  {'form':form })
 
 def book_edit(request):
-    pass
+  pass
 
 def book_delete(request):
-    pass
+  pass
