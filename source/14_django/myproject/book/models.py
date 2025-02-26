9b1c896f8537861565d69b4dd49d903c57997880
KHH618 <dlow123@naver.com>
2025-02-26 13:43:54 +0900

    2025-02-26

--- source/14_django/myproject/book/models.py
+++ source/14_django/myproject/book/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django import forms
+from django.urls import reverse
 from django.core.validators import MinLengthValidator
 from django.core.validators import MinValueValidator
 from django.core.validators import MaxValueValidator
@@ -23,6 +24,9 @@ class Book(models.Model):
                                   validators=[MinValueValidator(0),
                                               MaxValueValidator(1000000)])
   ip        = models.GenericIPAddressField(blank=True, null=True)
+
+  def get_absolute_url(self):
+      return reverse('book:list')
 
   def __str__(self):
     return "{}:{}著 {:,}원 from{}".format(self.title,
--- source/14_django/myproject/book/views.py
+++ source/14_django/myproject/book/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.db.transaction import commit
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .forms import BookForm, BookModelForm
 from .models import Book
@@ -7,21 +8,24 @@ from django.views.generic import CreateView
 from django.views.generic import UpdateView
 from django.views.generic import DeleteView
 
-book_list = ListView.as_view(model=Book)
-# def book_list(request):
-#   return render(request,
-#                 'book/book_list.html',
-#                 {'book_list':Book.objects.all()})
+book_list = ListView.as_view(Book)
+def book_list(request):
+  return render(request,
+                'book/book_list.html',
+                {'book_list':Book.objects.all()})
 
 def book_new(request):
   if request.method == 'POST':
     # request.POST의 파라미터 값을 book으로 save()
-    form = BookModelForm(request.POST, request.FILES)
+    form = BookModelForm(request.POST)
     if form.is_valid():
-      book = Book(**form.cleaned_data)
+      # book = Book(**form.cleaned_data)
+      book = form.save(commit=False)
       book.ip = request.META['REMOTE_ADDR'] # 요청한 client의 ip
       book.save()
-      return redirect('book:list')
+      return redirect(book)
+      # return redirect('book:list')
+
   else:
     #form = BookForm()
     form = BookModelForm()
@@ -29,8 +33,28 @@ def book_new(request):
                   'book/book_form.html',
                   {'form':form })
 
-def book_edit(request):
-  pass
+def book_edit(request, pk):
+  book = get_object_or_404(Book, pk=pk)
+  print('수정될 책 정보', book)
+  if request.method == 'POST':
+    # 파라미터 정보를 form객체로 받아 유효성 체크 (1)성공시 save (2)실패시 form태그
+    form = BookModelForm(request.POST, instance=book)
+    if form.is_valid():
+      book = form.save(commit=False)
+      book.save()
+      return redirect(book)
+  else:
+    form = BookModelForm(instance=book)
+  return render(request,
+        'book/book_form.html',
+        {'form':form })
 
-def book_delete(request):
-  pass
+def book_delete(request, pk):
+  book = get_object_or_404(Book, pk=pk)
+  if request.method == 'POST':
+    book.delete()
+    return redirect(book)
+  else:
+    return render(request,
+                  'book/book_confirm_delete.html',
+                  {'object':book})
